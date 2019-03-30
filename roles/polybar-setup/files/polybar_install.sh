59947aaf01442cb25d09fbca9118d24187b9d815
Allen Kirby <17akirby@gmail.com>
2019-03-30 10:00:49 -0400

    add siji installation to polybar install script

--- roles/polybar-setup/files/polybar_install.sh
+++ roles/polybar-setup/files/polybar_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 POLYBAR_PARENT_FOLDER=~/Git
+
+# install siji font first
+cd $POLYBAR_PARENT_FOLDER/siji
+./install.sh
+sudo dpkg-reconfigure fontconfig-config
+sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf && fc-cache
+
 cd $POLYBAR_PARENT_FOLDER/polybar
 mkdir build && cd build
 cmake ..
